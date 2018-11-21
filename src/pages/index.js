3d7344b350fe289a75f712442b8cb69587175b2d
gowtham.mk <gowtham.mk@zohocorp.com>
2018-11-21 22:19:35 +0530

    Updates the form with custom subject

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,6 +122,7 @@ class HomeIndex extends React.Component {
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" data-netlify="true">
+                                    <input type="hidden" name="subject" value="Form submitted from gowthamrm website" />
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
